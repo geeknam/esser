25c8a84fe589640d19f168ac8d7d6e37ade38aa2
Nam Ngo <nam@merlingroup.co>
2017-03-25 16:45:06 +1100

    Add setup.py and use Travid deploy to upload to pypi.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(
+    name='esser',
+    packages=['esser'],
+    license='Apache 2.0',
+    version='0.1.0',
+    description='Python Event Sourcing framework',
+    long_description=open('README.md').read(),
+    author='Nam Ngo',
+    author_email='namngology@gmail.com',
+    url='https://geeknam.github.io/esser',
+    keywords=[
+        'event sourcing', 'framework', 'esser', 'serverless',
+        'dynamodb', 'lambda'
+    ],
+    install_requires=['pynamodb', 'cerberus']
+)
